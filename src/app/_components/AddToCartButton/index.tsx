92026c3de5b283e26f7a38695724d5edec0c3aa4
Bruno Correia <brunovpm@hotmail.com>
2024-03-10 18:02:12 -0300

    traducao carrinho

--- src/app/_components/AddToCartButton/index.tsx
+++ src/app/_components/AddToCartButton/index.tsx
@@ -30,7 +30,7 @@ export const AddToCartButton: React.FC<{
     <Button
       href={isInCart ? '/cart' : undefined}
       type={!isInCart ? 'button' : undefined}
-      label={isInCart ? `✓ View in cart` : `Add to cart`}
+      label={isInCart ? '✓ Ver no carrinho' : 'Adicionar ao carrinho'}
       el={isInCart ? 'link' : undefined}
       appearance={appearance}
       className={[
