41082478e8c021335e9f3fc9f147dc12e20a7c38
Bruno Correia <brunovpm@hotmail.com>
2024-03-24 13:53:23 -0300

    Mudança no header social medias e logo

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,35 +1,43 @@
-'use client'
+// HeaderComponent.jsx
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { HeaderNav } from '../Nav';
+import classes from './index.module.scss'// Garanta que o caminho está correto
 
-import { Header } from '../../../../payload/payload-types'
-import { noHeaderFooterUrls } from '../../../constants'
-import { Gutter } from '../../Gutter'
-import { HeaderNav } from '../Nav'
-
-import classes from './index.module.scss'
-
-const HeaderComponent = ({ header }: { header: Header }) => {
-  const pathname = usePathname()
-
+const HeaderComponent = ({ header }) => {
   return (
-    <nav
-      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
-        .filter(Boolean)
-        .join(' ')}
-    >
-      <Gutter className={classes.wrap}>
-        <Link href="/">
-          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+    <header className={classes.header}>
+      {/* Use o componente Link corretamente sem a tag <a> */}
+      <Link href="/">
+        <Image src="/minimo_1.jpeg" alt="TMínimo 1" width={350} height={80} className={classes.logo} />
+      </Link>
+
+      <HeaderNav header={header} />
+
+      <div className={classes.headerIcons}>
+        {/* Removido <a> de dentro do Link */}
+        <Link href="/products" className={classes.iconLink}>
+          <Image src="/search.png" alt="Search" width={100} height={100} />
         </Link>
+        <Link href="/orders" className={classes.iconLink}>
+          <Image src="/user_profile.png" alt="Orders" width={100} height={100} />
+        </Link>
+        <Link href="/cart" className={classes.iconLink}>
+          <Image src="/cart.png" alt="Cart" width={100} height={100} />
+        </Link>
+      </div>
+      <div className={classes.headerSocialIcons}>
+      <a href="https://www.instagram.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
+        <Image src="/instagram.png" alt="Instagram" width={80} height={80} />
+      </a>
+      <a href="https://www.tiktok.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
+        <Image src="/tiktok.png" alt="TikTok" width={80} height={80} />
+      </a>
+    </div>
+    </header>
+  );
+};
 
-        <HeaderNav header={header} />
-      </Gutter>
-    </nav>
-  )
-}
-
-export default HeaderComponent
+export default HeaderComponent;
