bdbcf11c51265f8e6e67e26eed62372963383f9b
Bruno Correia <brunovpm@hotmail.com>
2024-03-30 10:15:08 -0300

    Add nav para fazer os botoes de menu

---
+++ src/app/_components/Header/Nav/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+
+import { Header as HeaderType } from '../../../../payload/payload-types'
+import { useAuth } from '../../../_providers/Auth'
+import { Button } from '../../Button'
+import { CartLink } from '../../CartLink'
+import { CMSLink } from '../../Link'
+
+import classes from './index.module.scss'
+
+export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
+  const navItems = header?.navItems || []
+  const { user } = useAuth()
+
+  return (
+    <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
+      {navItems.map(({ link }, i) => {
+        return <CMSLink key={i} {...link} appearance="none" />
+      })}
+      <CartLink />
+      {user && <Link href="/account">Account</Link>}
+      {!user && (
+        <Button
+          el="link"
+          href="/login"
+          label="Login"
+          appearance="primary"
+          onClick={() => (window.location.href = '/login')}
+        />
+      )}
+      {user && <CartLink />}
+    </nav>
+  )
+}
