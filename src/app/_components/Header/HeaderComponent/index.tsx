1888e7911ba83af529591fe9694ed51bc37f7291
Bruno Correia <brunovpm@hotmail.com>
2024-03-30 12:27:05 -0300

    drop down menu on click

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,26 +1,97 @@
 // HeaderComponent.jsx
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-
-import { HeaderNav } from '../Nav';
+import Dropdown from 'react-simple-dropdown';
+// import { HeaderNav } from '../Nav';
 import classes from './index.module.scss'// Garanta que o caminho está correto
-
 const HeaderComponent = ({ header }) => {
+  const [dropdownStates, setDropdownStates] = useState({
+    peças: false,
+    emAlta: false,
+    sale: false,
+    newIn: false,
+  });
+
+  const toggleDropdown = (buttonName) => {
+    setDropdownStates((prevStates) => ({
+      ...prevStates,
+      [buttonName]: !prevStates[buttonName], // Toggle specific dropdown
+    }));
+  };
+
   return (
     <header className={classes.header}>
-      {/* Use o componente Link corretamente sem a tag <a> */}
+      {/* Use Link correctly without <a> */}
       <Link href="/">
         <Image src="/minimo_1.jpeg" alt="TMínimo 1" width={300} height={300} className={classes.logo} />
       </Link>
+
       <div className={classes.headerButtons}>
-        <a href="/products" className={classes.buttonLink}>New in</a>
-        <a href="/products" className={classes.buttonLink}>Peças</a>
-        <a href="/products" className={classes.buttonLink}>Em Alta</a>
-        <a href="/products" className={classes.buttonLink}>Sale</a>
+        {/* New in */}
+        <Link href="#" onClick={() => toggleDropdown('newIn')} className={classes.buttonLink}>
+          <span>New in</span>
+          {dropdownStates.newIn && (
+            <div className={classes.dropdownMenuVertical}>
+              <Link href="/products" className={classes.dropdownItem}>
+                A
+              </Link>
+              <Link href="/products" className={classes.dropdownItem}>
+                B
+              </Link>
+            </div>
+          )}
+        </Link>
+
+        {/* Peças */}
+        <Link href="#" onClick={() => toggleDropdown('peças')} className={classes.buttonLink}>
+        <span>Peças</span>
+        {dropdownStates.peças && (
+          <div className={classes.dropdownMenuVertical}> {/* Apply vertical positioning class */}
+            <Link href="/products" className={classes.dropdownItem}>
+              A
+            </Link>
+            <Link href="/products" className={classes.dropdownItem}>
+              B
+            </Link>
+          </div>
+        )}
+        </Link>
+
+        {/* Em Alta */}
+        <Link href="#" onClick={() => toggleDropdown('emAlta')} className={classes.buttonLink}>
+          <span>Em Alta</span>
+          {dropdownStates.emAlta && (
+            <div className={classes.dropdownMenuVertical}> {/* Use a different class for vertical positioning */}
+              <Link href="/products" className={classes.dropdownItem}>
+                A
+              </Link>
+              <Link href="/products" className={classes.dropdownItem}>
+                B
+              </Link>
+            </div>
+          )}
+        </Link>
+
+        {/* Sale */}
+        <Link href="#" onClick={() => toggleDropdown('sale')} className={classes.buttonLink}>
+          <span>Sale</span>
+          {dropdownStates.sale && (
+            <div className={classes.dropdownMenuVertical}>
+              <Link href="/products" className={classes.dropdownItem}>
+                A
+              </Link>
+              <Link href="/products" className={classes.dropdownItem}>
+                B
+              </Link>
+            </div>
+          )}
+        </Link>
       </div>
+
       <div className={classes.headerIcons}>
-        {/* Removido <a> de dentro do Link */}
+        {/* Removed <a> within Link */}
         <Link href="/products" className={classes.iconLink}>
           <Image src="/search.png" alt="Search" width={500} height={500} layout="fixed"/>
         </Link>
@@ -32,13 +103,13 @@ const HeaderComponent = ({ header }) => {
         </Link>
       </div>
       <div className={classes.headerSocialIcons}>
-      <a href="https://www.instagram.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
-        <Image src="/instagram.png" alt="Instagram" width={500} height={500} layout="fixed"/>
-      </a>
-      <a href="https://www.tiktok.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
-        <Image src="/tiktok.png" alt="TikTok" width={500} height={500} layout="fixed"/>
-      </a>
-    </div>
+        <a href="https://www.instagram.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
+          <Image src="/instagram.png" alt="Instagram" width={500} height={500} layout="fixed"/>
+        </a>
+        <a href="https://www.tiktok.com" className={classes.iconLink} target="_blank" rel="noopener noreferrer">
+          <Image src="/tiktok.png" alt="TikTok" width={500} height={500} layout="fixed"/>
+        </a>
+      </div>
     </header>
   );
 };
