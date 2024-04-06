6571ee090f1112bd5a4f2af56d1d3c306da009aa
Bruno Correia <brunovpm@hotmail.com>
2024-04-06 13:00:47 -0300

    remove compre por categorias e promocoes da pagina inicial

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -66,8 +66,8 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
 
           <Gutter className={classes.home}>
-            <Categories categories={categories} />
-            <Promotion />
+            {/* <Categories categories={categories} /> */}
+            {/* <Promotion /> */} 
           </Gutter>
         </section>
       ) : (
