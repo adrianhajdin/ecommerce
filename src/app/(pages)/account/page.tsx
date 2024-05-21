e462d034c40afbc5ceaccbc0866660b14dcea7d8
las-t1k <nicosathler@hotmail.com>
2024-05-21 00:14:35 -0300

    small changes

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -9,7 +9,7 @@ import classes from './index.module.scss'
 export default async function Account() {
   return (
     <div>
-      <h5 className={classes.personalInfo}>Personal Information</h5>
+      <h5 className={classes.personalInfo}>Seus Dados</h5>
       <AccountForm />
     </div>
   )
