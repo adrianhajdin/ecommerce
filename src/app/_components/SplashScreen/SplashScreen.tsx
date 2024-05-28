9d4157aa72da2800a277cee73f8c929f0cef0b8f
Bruno Correia <brunovpm@hotmail.com>
2024-05-28 16:21:32 -0300

    fix video intro

--- src/app/_components/SplashScreen/SplashScreen.tsx
+++ src/app/_components/SplashScreen/SplashScreen.tsx
@@ -4,7 +4,7 @@
 const SplashScreen: React.FC = () => {
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 9999, backgroundColor: 'black' }}>
-      <video src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/video_intro.mp4`} autoPlay muted style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      <video src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/intro.mp4`} autoPlay muted style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
     </div>
   );
 };
