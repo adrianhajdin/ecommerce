8717d9b24c043dbefb843c54d3be23b238c30575
Bruno Correia <brunovpm@hotmail.com>
2024-05-31 19:26:19 -0300

    Update page.tsx

--- src/app/(pages)/styleguide/media-block/page.tsx
+++ src/app/(pages)/styleguide/media-block/page.tsx
@@ -1,33 +1,14 @@
-import React, { Fragment } from 'react';
-import { Metadata } from 'next';
-import Link from 'next/link';
+import React, { Fragment } from 'react'
+import { Metadata } from 'next'
+import Link from 'next/link'
 
-import staticImage from '../../../../../public/static-image.jpg';
-import { MediaBlock } from '../../../_blocks/MediaBlock';
-import { Gutter } from '../../../_components/Gutter';
-import { VerticalPadding } from '../../../_components/VerticalPadding';
-import { mergeOpenGraph } from '../../../_utilities/mergeOpenGraph';
+import staticImage from '../../../../../public/static-image.jpg'
+import { MediaBlock } from '../../../_blocks/MediaBlock'
+import { Gutter } from '../../../_components/Gutter'
+import { VerticalPadding } from '../../../_components/VerticalPadding'
+import { mergeOpenGraph } from '../../../_utilities/mergeOpenGraph'
 
-export async function getStaticProps() {
-  let media = null;
-
-  try {
-    const res = await fetch('https://api.exemplo.com/media');
-    if (res.ok) {
-      media = await res.json();
-    }
-  } catch (error) {
-    console.error('Failed to fetch media:', error);
-  }
-
-  return {
-    props: {
-      media: media || null,
-    },
-  };
-}
-
-const MediaBlockPage = ({ media }) => {
+export default async function MediaBlockPage() {
   return (
     <Fragment>
       <Gutter>
@@ -39,26 +20,19 @@ const MediaBlockPage = ({ media }) => {
         <h1>Media Block</h1>
       </Gutter>
       <VerticalPadding bottom="large" top="none">
-        <MediaBlock 
-          position="default" 
-          blockType="mediaBlock" 
-          media={media} 
-          staticImage={staticImage} 
-        />
+        <MediaBlock position="default" blockType="mediaBlock" media="" staticImage={staticImage} />
         <br />
         <br />
         <MediaBlock
           position="fullscreen"
           blockType="mediaBlock"
-          media={media}
+          media=""
           staticImage={staticImage}
         />
       </VerticalPadding>
     </Fragment>
-  );
+  )
 }
-
-export default MediaBlockPage;
 
 export const metadata: Metadata = {
   title: 'Media Block',
@@ -67,4 +41,4 @@ export const metadata: Metadata = {
     title: 'Media Block',
     url: '/styleguide/media-block',
   }),
-};
+}
