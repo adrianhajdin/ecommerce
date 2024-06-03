d23f5e401861a0d270df3597c5a639d68bd0e396
las-t1k <nicosathler@hotmail.com>
2024-06-03 02:44:59 -0300

    linter

--- src/app/_components/ExportButton/index.tsx
+++ src/app/_components/ExportButton/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Pill } from 'payload/components' 
+import { Pill } from 'payload/components'
 import * as XLSX from 'xlsx'
 
 const ExportButton = ({ data }) => {
-  console.log(data); // Log data for debugging or inspection
+  console.log(data) // Log data for debugging or inspection
 
   const handleClick = () => {
-    const worksheet = XLSX.utils.json_to_sheet(data.docs);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-    XLSX.writeFile(workbook, 'Data_Export.xlsx');
+    const worksheet = XLSX.utils.json_to_sheet(data.docs)
+    const workbook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+    XLSX.writeFile(workbook, 'Data_Export.xlsx')
   }
 
   return (
