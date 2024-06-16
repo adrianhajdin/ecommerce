a421a1bbd015df72a040f8eaf121a460386f4a5b
Bruno Correia <brunovpm@hotmail.com>
2024-06-16 00:38:09 -0300

    fix gutter da low impact

--- src/app/_heros/LowImpact/index.tsx
+++ src/app/_heros/LowImpact/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react'
-
-import { Page } from '../../../payload/payload-types'
 import { Gutter } from '../../_components/Gutter'
+import { Page } from '../../../payload/payload-types'
+import React from 'react'
 import RichText from '../../_components/RichText'
 import { VerticalPadding } from '../../_components/VerticalPadding'
-
 import classes from './index.module.scss'
 
 export const LowImpactHero: React.FC<Page['hero']> = ({ richText }) => {
   return (
     <Gutter className={classes.lowImpactHero}>
       <div className={classes.content}>
-        <VerticalPadding>
           <RichText className={classes.richText} content={richText} />
-        </VerticalPadding>
       </div>
     </Gutter>
   )
