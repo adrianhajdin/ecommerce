c41e5fabb2d59c70419152aa8a13d2628ec14acf
Bruno Correia <brunovpm@hotmail.com>
2024-06-16 23:21:10 -0300

    Update index.tsx

--- src/app/_providers/Filter/index.tsx
+++ src/app/_providers/Filter/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode, SetStateAction, createContext, useContext, useState } from 'react'
+import { createContext, ReactNode, SetStateAction, useContext, useState } from 'react'
 
 interface IContextType {
   categoryFilters: string[]
