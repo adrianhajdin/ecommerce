76f830279dd18034736c6250b9273cb8a5fa4ea1
Aiman <aiman.n@hotmail.fr>
2024-11-25 02:43:52 +0100

    Ajout de nouvelles critiques avec des évaluations et des commentaires, mise à jour du style d'affichage des critiques

--- src/app/_components/_Vercelreviewstshirt/reviews.tsx
+++ src/app/_components/_Vercelreviewstshirt/reviews.tsx
@@ -9,15 +9,26 @@ export function Reviews() {
         Recent reviews
       </h2>
 
-      <div className="mt-6 space-y-10 divide-y divide-gray-200 border-b border-t border-gray-200 pb-10">
+      <div className="mt-6 space-y-10 divide-y divide-gray-200 border-b border-t border-gray-200 pb-5">
         {reviews.featured.map((review) => (
           <div
             key={review.id}
-            className="pt-10 lg:grid lg:grid-cols-12 lg:gap-x-8"
+            className="pt-5 lg:grid lg:grid-cols-12 lg:gap-x-8"
           >
             <div className="lg:col-span-8 lg:col-start-5 xl:col-span-9 xl:col-start-4 xl:grid xl:grid-cols-3 xl:items-start xl:gap-x-8">
               <div className="flex items-center xl:col-span-1">
                 <div className="flex items-center">
+                <div className="mt-6 flex items-center text-sm lg:col-span-4 lg:col-start-1 lg:row-start-1 lg:mt-0 lg:flex-col lg:items-start xl:col-span-3">
+                <p className="font-medium text-gray-900">{review.author}</p>
+                <time
+                  dateTime={review.datetime}
+                  className="ml-4 border-l border-gray-200 pl-4 text-gray-500 lg:ml-0 lg:mt-2 lg:border-0 lg:pl-0"
+                >
+                  {review.date}
+                </time>
+              </div>
+                  
+                  
                   {[0, 1, 2, 3, 4].map((rating) => (
                     <StarIcon
                       key={rating}
@@ -25,7 +36,7 @@ export function Reviews() {
                         review.rating > rating
                           ? 'text-yellow-400'
                           : 'text-gray-200',
-                        'h-5 w-5 flex-shrink-0',
+                        'h-10 w-10 flex-shrink-0',
                       )}
                       aria-hidden="true"
                     />
